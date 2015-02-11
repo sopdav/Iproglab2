3e174c93cb41dc64cc36d4a12663083fa1778af5
sopdav <sopdav@kth.se>
2015-02-11 12:04:04 +0100

    sophie

--- dinnerplanner-html/js/view/exampleView.js
+++ dinnerplanner-html/js/view/exampleView.js
@@ -4,6 +4,7 @@ var ExampleView = function (container) {
 	// Get all the relevant elements of the view (ones that show data
   	// and/or ones that responed to interaction)
 	this.numberOfGuests = container.find("#numberOfGuests");
+
 	this.plusButton = container.find("#plusGuest");
 	this.minusButton = container.find("#minusGuest");
 	
---
+++ dinnerplanner-html/js/view/selectDishView.js
@@ -0,0 +1,5 @@
+var selectDishView = function (container){
+	
+
+	
+}
