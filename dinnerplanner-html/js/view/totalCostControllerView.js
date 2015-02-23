55a5f589911c3e0fe626d24d7bb7c55551780df7
idava <idava@kth.se>
2015-02-23 17:32:28 +0100

    ida

--- dinnerplanner-html/js/view/totalCostControllerView.js
+++ dinnerplanner-html/js/view/totalCostControllerView.js
@@ -1,3 +1,8 @@
 var TotalCostControllerView = function(view, model) {
-	
+
+	view.printFullRecipeButton.click(function() {
+		$('#totalCostView').hide();
+		$('#lasagneView').hide();
+		$('#finalView').show();
+	});
 }
--- dinnerplanner-html/js/view/totalCostView.js
+++ dinnerplanner-html/js/view/totalCostView.js
@@ -24,7 +24,34 @@ var TotalCostView = function(container, model) {
 
 			if (dish.type == 'starter') {
 				this.starter.attr('scr' + 'images/' + dish.image);
+				this.starterName.html(dish.name);
+				this.starterCost.html(model.getDishCost(dish) + ' SEK');
+			};
+
+			if (dish.type == 'main dish') {
+				this.main.attr('scr' + 'images/' + dish.image);
+				this.mainName.html(dish.name);
+				this.mainCost.html(model.getDishCost(dish) + ' SEK');
+			};
+
+			if (dish.type == 'dessert') {
+				this.dessert.attr('scr' + 'images/' + dish.image);
+				this.dessertName.html(dish.name);
+				this.dessertCost.html(model.getDishCost(dish) + ' SEK');
 			};
 		};
+	this.totalCost.html(model.getTotalMenuPrice());
 	}
+
+	/*****
+
+		The observer part
+
+		We register an observer and then set the initial values
+
+	*****/
+
+	model.addObserver(this);
+
+	this.update();
 }
