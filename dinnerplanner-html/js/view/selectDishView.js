2e4c77f6fcb5526395954b70fc0848807439db1d
idava <idava@kth.se>
2015-02-23 10:18:42 +0100

    ida

--- dinnerplanner-html/js/view/selectDishView.js
+++ dinnerplanner-html/js/view/selectDishView.js
@@ -1,5 +1,9 @@
-var selectDishView = function (container){
-	
+var selectDishView = function (container, model){
+	// Here we load the dishes
+	this.starter = container.find("#starters");
+	this.main = container.find("mains");
+	this.dessert = container.find("desserts");
+
 
 	
 }
