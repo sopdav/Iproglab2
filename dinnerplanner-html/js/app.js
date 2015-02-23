1e7d8099d6a7bf1cab5dd9041a05b788118a9b9c
idava <idava@kth.se>
2015-02-23 10:02:05 +0100

    ida

--- dinnerplanner-html/js/app.js
+++ dinnerplanner-html/js/app.js
@@ -6,6 +6,7 @@ $(function() {
 	//And create the needed controllers and views
 	var exampleView = new ExampleView($("#exampleView"), model);
 
-
+	// Controllers for Select Dish View
+	var selectDishView = new selectDishView($(#selectDishView), model);
 
 });
