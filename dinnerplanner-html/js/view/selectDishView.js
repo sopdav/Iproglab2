ffec427be4d0e01de20e9d5de165aaf4efee87b9
idava <idava@kth.se>
2015-02-23 15:08:18 +0100

    ida

--- dinnerplanner-html/js/view/selectDishView.js
+++ dinnerplanner-html/js/view/selectDishView.js
@@ -4,6 +4,24 @@ var SelectDishView = function (container, model){
 	this.main = container.find("mains");
 	this.dessert = container.find("desserts");
 
+	var listOfSessions = "";
 
-	
+	for (var j = 0; j < model.getAllDishes("starter").length; j++) {
+			listOfSessions += '<li id=thelist style="display:inline-block; width: 200px; height:200px;">' + 
+			'<div id=' 
+				+ model.getAllDishes("starter")[j].id 
+				+ '>' 
+				+ '<img style="width: 150px; height: 150px;" src="' 
+					+ 'images/' 
+					+ model.getAllDishes("starter")[j].image 
+					+ '" alt ="' 
+					+ model.getAllDishes("starter")[j].image 
+					+ '">' 
+				+ '<br>' 
+				+ model.getAllDishes("starter")[j].name 
+			+ '</div>';
+
+		document.getElementById("starters").innerHTML = listOfSessions;
+		listOfSessions = "";
+		};
 }
