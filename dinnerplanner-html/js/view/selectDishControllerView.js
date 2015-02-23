3dc39fc9c31987111c65e76ed358b855d0115167
idava <idava@kth.se>
2015-02-23 15:53:18 +0100

    ida

--- dinnerplanner-html/js/view/selectDishControllerView.js
+++ dinnerplanner-html/js/view/selectDishControllerView.js
@@ -1,3 +1,32 @@
 var SelectDishControllerView = function(view, model) {
-	$(document)
+	$(document).on("change", "#option", function() {
+		var chosen = this.value;
+
+		// shows only starters
+		if (chosen == "Starters") { // looks over in the html-code to find the value
+			$('#starters').show();
+			$('#mains').hide();
+			$('#desserts').hide();
+		};
+		// shows only main courses
+		else if (chosen == "Main course") {
+			$('#starters').hide();
+			$('#mains').show();
+			$('desserts').hide();
+		};
+		// shows only desserts
+		else if (chosen == "Desserts") {
+			$('#starters').hide();
+			$('#mains').hide();
+			$('#desserts').show();
+		};
+		// shows all courses
+		else (chosen == "All") {
+			$('#starters').show();
+			$('#mains').show();
+			$('#desserts').show();
+		}
+	});
+
+	
 }
--- dinnerplanner-html/js/view/selectDishView.js
+++ dinnerplanner-html/js/view/selectDishView.js
@@ -6,22 +6,53 @@ var SelectDishView = function (container, model){
 
 	var listOfSessions = "";
 
+	// gets all the starter dishes with their picture and namne, li creates a list (bulleted?)
 	for (var j = 0; j < model.getAllDishes("starter").length; j++) {
 			listOfSessions += '<li id=thelist style="display:inline-block; width: 200px; height:200px;">' + 
 			'<div id=' 
-				+ model.getAllDishes("starter")[j].id 
-				+ '>' 
-				+ '<img style="width: 150px; height: 150px;" src="' 
-					+ 'images/' 
-					+ model.getAllDishes("starter")[j].image 
-					+ '" alt ="' 
-					+ model.getAllDishes("starter")[j].image 
-					+ '">' 
-				+ '<br>' 
-				+ model.getAllDishes("starter")[j].name 
+				+ model.getAllDishes("starter")[j].id + '>' 
+				+ '<img style="width: 150px; height: 150px;" src="' + 'images/' + model.getAllDishes("starter")[j].image 
+				+ '" alt ="' + model.getAllDishes("starter")[j].image + '">' + '<br>' + model.getAllDishes("starter")[j].name 
 			+ '</div>';
-
+		};
 		document.getElementById("starters").innerHTML = listOfSessions;
 		listOfSessions = "";
+
+	// gets all the main course dishes with their picture and namne
+	for (var k = 0; k < model.getAllDishes("starter").length; k++) {
+			listOfSessions += '<li id=thelist style="display:inline-block; width: 200px; height:200px;">' + 
+			'<div id=' 
+				+ model.getAllDishes("main")[k].id + '>' 
+				+ '<img style="width: 150px; height: 150px;" src="' + 'images/' + model.getAllDishes("main")[k].image 
+					+ '" alt ="' + model.getAllDishes("main")[k].image + '">' + '<br>' + model.getAllDishes("main")[k].name 
+			+ '</div>';
+			listOfSessions += '<br><br></li>'	// adds to the current list
 		};
+		document.getElementById("mains").innerHTML = listOfSessions;
+		listOfSessions = "";
+
+	// gets all the dessert dishes with their picture and namne
+	for (var l = 0; l < model.getAllDishes("dessert").length; l++) {
+			listOfSessions += '<li id=thelist style="display:inline-block; width: 200px; height:200px;">' + 
+			'<div id=' 
+				+ model.getAllDishes("dessert")[l].id + '>' 
+				+ '<img style="width: 150px; height: 150px;" src="' + 'images/' + model.getAllDishes("dessert")[l].image 
+				+ '" alt ="' + model.getAllDishes("dessert")[l].image + '">' + '<br>' + model.getAllDishes("dessert")[l].name 
+			+ '</div>';
+			listOfSessions += '<br><br></li>'	// adds to the current list
+		};
+		document.getElementById("desserts").innerHTML = listOfSessions;
+		listOfSessions = "";
+
+	// Create the function to search in the main bar
+
+	var searchList = [];
+
+	// gets the variables from searchList if they are in searchDish
+	$('#searchDish').typeahead( {
+		local: searchList;
+	});
+
+	
 }
+
