aecf742a628018a30d0240b49577e5d9a7a97627
sopdav <sopdav@kth.se>
2015-02-24 11:42:21 +0100

    sophie

--- dinnerplanner-html/js/view/selectDishView.js
+++ dinnerplanner-html/js/view/selectDishView.js
@@ -10,7 +10,7 @@ var SelectDishView = function (container, model){
 	for (var j = 0; j < model.getAllDishes("starter").length; j++) {
 			listOfSessions += '<li id=list style="display:inline-block;width:200px;height:200px;">' + '<div id=' + model.getAllDishes("starter")[j].id + '>' + '<img style="width:150px;height:150px;" src="' + 'images/' + model.getAllDishes("starter")[j].image + '" alt ="' + model.getAllDishes("starter")[j].image + 
 			'">' + '<br>' + model.getAllDishes("starter")[j].name + '</div>';
-			listOfSessions += '</li>';
+			listOfSessions += '<br><br></li>';
 		}
 		document.getElementById("starters").innerHTML = listOfSessions;
 		listOfSessions = "";
